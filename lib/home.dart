48f44f809e69559855c7244e28e5dbcc537d44a7
ASM <s.touazi@eleves-alsacienne.org>
2021-03-20 22:07:11 +0100

    navbar

--- lib/home.dart
+++ lib/home.dart
@@ -7,6 +7,7 @@ void main() {
 }
 
 
+
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -158,8 +159,11 @@ class Consignes extends StatelessWidget {
                     ),
                   ),
                 SizedBox(
+                  height: MediaQuery.of(context).size.height/7,
+                ),
+                SizedBox(
                   width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height/5,
+                  height: MediaQuery.of(context).size.height/10,
                   child: Container(
                     alignment: Alignment.bottomCenter,
                     child: SizedBox(
@@ -192,11 +196,33 @@ class Consignes extends StatelessWidget {
 
 
 
+
 class Menu extends StatelessWidget {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold, decorationColor: Color.fromARGB(255, 245, 127, 23),);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 1: Context',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: Flutter',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 3: React Native',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 4: Comparaison',
+      style: optionStyle,
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter layout demo',
+        title: 'Menu',
         theme: ThemeData(
             primaryColor: const Color(0xFF6B62A3)
         ),
@@ -204,84 +230,212 @@ class Menu extends StatelessWidget {
             backgroundColor: Colors.white,
             body:
             Column(
-              children: <Widget>[
-                SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height/5,
-                  child: Container(
-                    alignment: Alignment.bottomCenter,
-                    child: SizedBox(
-                      width: MediaQuery.of(context).size.width/2,
-                      height: MediaQuery.of(context).size.height/10,
-                      child: Tooltip(
-                        message: "Context",
-
-                        child: (TextButton(
-                          style: TextButton.styleFrom(
-                            primary: Colors.deepPurple[600],
-                          ),
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          child: Text("1. Qu'est-ce qu'un outil cross-plateforme",
-                            style: TextStyle (
-                                fontSize: 25),
-                          ),
-                        )
-                        ),
+                children: <Widget>[
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height/5,
+                    child: Center(
+                      child: Text(
+                        'Menu :',
+                        style: TextStyle (
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.deepPurple[600]),
                       ),
                     ),
                   ),
-                ),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width/1.05,
-                  height: MediaQuery.of(context).size.height/2.8,
-                  child: Container(
-                    alignment: Alignment.bottomCenter,
-                    child: SizedBox(
-                      width: MediaQuery.of(context).size.width/2,
-                      height: MediaQuery.of(context).size.height/10,
-                      child: (ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            primary: Colors.yellow[900]),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height/7,
+                    child: Tooltip(
+                      message: "Context",
+                      child: (TextButton(
+                        style: TextButton.styleFrom(
+                          primary: Colors.deepPurple[600],
+                        ),
                         onPressed: () {
                           Navigator.pop(context);
                         },
-                        child: Text("C'est parti !",
+                        child: Text("1. Qu'est-ce qu'un outil cross-plateforme",
                           style: TextStyle (
-                              fontSize: 25),
+                              decoration: TextDecoration.underline,
+                              fontSize: 20),
                         ),
                       )
                       ),
                     ),
                   ),
-                ),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height/5,
-                  child: Container(
-                    alignment: Alignment.bottomCenter,
-                    child: SizedBox(
-                      width: MediaQuery.of(context).size.width/2,
-                      height: MediaQuery.of(context).size.height/10,
-                      child: (ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            primary: Colors.yellow[900]),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height/7,
+                    child: Tooltip(
+                      message: "Context",
+                      child: (TextButton(
+                        style: TextButton.styleFrom(
+                          primary: Colors.deepPurple[600],
+                        ),
                         onPressed: () {
                           Navigator.pop(context);
                         },
-                        child: Text("C'est parti !",
+                        child: Text("2. Flutter",
                           style: TextStyle (
-                              fontSize: 25),
+                              decoration: TextDecoration.underline,
+                              fontSize: 20),
                         ),
                       )
                       ),
                     ),
                   ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height/7,
+                    child: Tooltip(
+                      message: "Context",
+                      child: (TextButton(
+                        style: TextButton.styleFrom(
+                          primary: Colors.deepPurple[600],
+                        ),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text("3. React Native",
+                          style: TextStyle (
+                              decoration: TextDecoration.underline,
+                              fontSize: 20),
+                        ),
+                      )
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height/7,
+                    child: Tooltip(
+                      message: "Context",
+                      child: (TextButton(
+                        style: TextButton.styleFrom(
+                          primary: Colors.deepPurple[600],
+                        ),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => Bar()),
+                          );
+                        },
+                        child: Text("4. Etude Comparative",
+                          style: TextStyle (
+                              decoration: TextDecoration.underline,
+                              fontSize: 20),
+                        ),
+                      )
+                      ),
+                    ),
+                  ),
+                  FloatingActionButton.extended(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Home()),
+                      );
+                    },
+                    icon: Icon(Icons.home),
+                    label: Text("Home"),
+                    backgroundColor: Colors.yellow[900],
+                  ),
+                ]
+            ),
+          bottomNavigationBar: BottomNavigationBar(
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.ad_units_outlined),
+                label: 'Context',
+              ),
+              BottomNavigationBarItem(
+                icon: ImageIcon( AssetImage('asset/flutter.png'),
                 ),
-              ],
-            )
+                label: 'Flutter',
+              ),
+              BottomNavigationBarItem(
+                icon: ImageIcon(
+                  AssetImage('asset/react_native.png'),
+                ),
+                label: 'React',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.analytics_outlined),
+                label: 'Comparaison',
+              ),
+            ],
+            currentIndex: _selectedIndex,
+            selectedItemColor: Colors.deepPurple[600],
+          ),
         )
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+class Bar extends StatelessWidget {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold, decorationColor: Color.fromARGB(255, 245, 127, 23),);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 1: Context',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: Flutter',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 3: React Native',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 4: Comparaison',
+      style: optionStyle,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.ad_units_outlined),
+            label: 'Context',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon( AssetImage('asset/flutter.png'),
+            ),
+            label: 'Flutter',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage('asset/react_native.png'),
+            ),
+            label: 'React',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.analytics_outlined),
+            label: 'Comparaison',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.deepPurple[600],
+      ),
     );
   }
 }
