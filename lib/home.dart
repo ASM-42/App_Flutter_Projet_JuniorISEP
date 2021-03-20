cd334124668e52f18f0b814247fe266aad60a9f5
ASM <s.touazi@eleves-alsacienne.org>
2021-03-20 05:30:33 +0100

    Consignes

--- lib/home.dart
+++ lib/home.dart
@@ -10,10 +10,14 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter layout demo',
-      home: Scaffold(
-        body:
-        Column(
+      title: 'Home',
+        theme: ThemeData(
+            primaryColor: const Color(0xFF6B62A3)
+        ),
+      home: //Scaffold(
+        Builder(
+        builder: (context) => Scaffold(
+        body: Column(
           children: <Widget>[
             Padding(padding: EdgeInsets.symmetric(vertical : 100.0),
               child: (Image.asset('asset/JE_complet.png', fit: BoxFit.cover,)
@@ -56,6 +60,7 @@ class Home extends StatelessWidget {
         ],
     )
     )
+    )
     );
   }
 }
@@ -66,43 +71,46 @@ class Home2 extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Flutter layout demo',
+        theme: ThemeData(
+          primaryColor: const Color(0xFF6B62A3)
+        ),
         home: Scaffold(
             body:
             Column(
               children: <Widget>[
-                Padding(padding: EdgeInsets.symmetric(vertical : 100.0),
-                    child: (Image.asset('asset/JE_complet.png', fit: BoxFit.cover,)
-                    )
-                ),
-                Padding(padding: EdgeInsets.all(16.0),
+                Padding(padding: EdgeInsets.only(top: 95.0, bottom: 50.0),
                   child: Text(
-                    'Projet Recrutement n°3',
+                    'Consignes du projet :',
                     style: TextStyle (
                         fontSize: 25,
-                        fontWeight: FontWeight.bold),
+                        fontWeight: FontWeight.bold,
+                        color: Colors.deepPurple[600]),
                   ),
+                ),
+                Padding(padding: EdgeInsets.all(50.0),
+                  child: RichText(
+                    textAlign: TextAlign.justify,
+                    text: TextSpan(
+                      text: 'Comparer la nouvelle version de Flutter aux autres outils permettant de créer des applications mobiles cross-platform, disponibles sur le marché. Flutter serait-elle une solution plus adaptée que React Native ppour le travail de Junior ISEP?',
+                      style: TextStyle(
+                        fontSize : 20,
+                        color: Colors.black,
+                      ),
+                    ),
+                  )
                 ),
                 Padding(padding: EdgeInsets.symmetric(vertical: (70.0)),
                   child: (ElevatedButton(
                     style: ElevatedButton.styleFrom(
                         primary: Colors.yellow[900]),
                     onPressed: () {
-                      // Respond to button press
+                      Navigator.pop(context);
                     },
                     child: Text('Go Back',
                       style: TextStyle (
                           fontSize: 25),
                     ),
                   )
-                  ),
-                ),
-                Padding(padding: EdgeInsets.only(top: 150.0),
-                  child: Text(
-                    'Powered by ASM',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontStyle: FontStyle.italic,
-                      color : Colors.deepPurple[600],),
                   ),
                 ),
               ],
