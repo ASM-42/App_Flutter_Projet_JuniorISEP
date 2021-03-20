2cc05ba351fec3c8aaf8e1b97047ecfe73088e76
ASM <s.touazi@eleves-alsacienne.org>
2021-03-20 16:37:31 +0100

    Size reactive + Border 2nd Page

--- lib/home.dart
+++ lib/home.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 void main() {
   runApp(Home());
 }
-// violet = #6B62A3
-// orange = #DA6436
-// jaune =
+
+
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //MediaQueryData queryData = MediaQuery.of(context);
     return MaterialApp(
       title: 'Home',
         theme: ThemeData(
@@ -18,26 +19,31 @@ class Home extends StatelessWidget {
         Builder(
         builder: (context) => Scaffold(
         body: Column(
+          mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            AspectRatio(aspectRatio: 1.0,
-              child: (Image.asset('asset/JE_complet.png', width: 100, height: 50,)
-              )
-              ),
-            AspectRatio(aspectRatio: 5.5,
-              child: Container(
-                width: 100,
-                height: 10,
-                child: Text(
-                    'Projet Recrutement n°3',
-                    style: TextStyle (
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,),
-                      textAlign: TextAlign.center,
-                      ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height/2,
+              child: (Image.asset('asset/JE_complet.png',
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height*50,)
               ),
             ),
             SizedBox(
-              width: 150,
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height/10,
+              child: Text(
+                  'Projet Recrutement n°3',
+                  style: TextStyle (
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,),
+                    textAlign: TextAlign.center,
+                    ),
+            ),
+
+            SizedBox(
+              width: MediaQuery.of(context).size.width/2,
+              height: MediaQuery.of(context).size.height/10,
               child: (ElevatedButton(
                   style: ElevatedButton.styleFrom(
                     primary: Colors.yellow[900],),
@@ -54,11 +60,13 @@ class Home extends StatelessWidget {
               )
               ),
             ),
-            AspectRatio(aspectRatio: 2.0,
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height/7,
               child: Container(
                 alignment: Alignment.bottomCenter,
                 child: Text(
-                    'Powered by ASM',
+                    'Powered by Flutter',
                     style: TextStyle(
                         fontSize: 20,
                         fontStyle: FontStyle.italic,
@@ -67,6 +75,14 @@ class Home extends StatelessWidget {
 
               ),
             ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height/10,
+              child: Container(
+                alignment: Alignment.bottomCenter,
+                child: Image.asset('asset/flutter.png', scale: 24,),
+              ),
+            )
         ],
     )
     )
@@ -75,7 +91,17 @@ class Home extends StatelessWidget {
   }
 }
 
-
+BoxDecoration myBoxDecoration() {
+  return BoxDecoration(
+    border: Border.all(
+      width: 2,
+      color: Colors.deepPurple[600],
+    ),
+  borderRadius: BorderRadius.all(
+    Radius.circular(5.0),
+  )
+  );
+}
 class Home2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -88,39 +114,55 @@ class Home2 extends StatelessWidget {
             body:
             Column(
               children: <Widget>[
-                AspectRatio(aspectRatio: 1/7,
-                  child: Text(
-                    'Consignes du projet :',
-                    style: TextStyle (
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.deepPurple[600]),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height/5,
+                  child: Center(
+                    child: Text(
+                      'Consignes du projet :',
+                      style: TextStyle (
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.deepPurple[600]),
+                    ),
                   ),
                 ),
-                AspectRatio(aspectRatio: 4/7,
-                  child: RichText(
-                    textAlign: TextAlign.justify,
-                    text: TextSpan(
-                      text: 'Comparer la nouvelle version de Flutter aux autres outils permettant de créer des applications mobiles cross-platform, disponibles sur le marché. Flutter serait-elle une solution plus adaptée que React Native ppour le travail de Junior ISEP?',
-                      style: TextStyle(
-                        fontSize : 20,
-                        color: Colors.black,
+                SizedBox(
+                  width: MediaQuery.of(context).size.width/1.1,
+                  height: MediaQuery.of(context).size.height/2,
+                  child: Container(
+                    margin: const EdgeInsets.all(2.0),
+                    decoration: myBoxDecoration(),
+                    child: Center(
+                      child: RichText(
+                        textAlign: TextAlign.justify,
+                        text: TextSpan(
+                          text: 'Comparer la nouvelle version de Flutter aux autres outils permettant de créer des applications mobiles cross-platform, disponibles sur le marché. Flutter serait-elle une solution plus adaptée que React Native ppour le travail de Junior ISEP?',
+                          style: TextStyle(
+                            fontSize : 20,
+                            color: Colors.black,
+                          ),
+                        ),
                       ),
                     ),
-                  )
+                  ),
                 ),
-                AspectRatio(aspectRatio: 2/7,
-                  child: (ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        primary: Colors.yellow[900]),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: Text('Go Back',
-                      style: TextStyle (
-                          fontSize: 25),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height/10,
+                  child: Center(
+                    child: (ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.yellow[900]),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text('Go Back',
+                        style: TextStyle (
+                            fontSize: 25),
+                      ),
+                    )
                     ),
-                  )
                   ),
                 ),
               ],
