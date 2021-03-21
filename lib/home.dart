4de2dea01ef5662960f1a2450d514339b54db5a6
ASM <s.touazi@eleves-alsacienne.org>
2021-03-21 21:30:41 +0100

    Final Commit

--- lib/home.dart
+++ lib/home.dart
@@ -459,10 +459,52 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
                             "Cette plateforme es diponible en open source, et ce depuis sa première version en 2018 et met à disposition une grande variété de "
                             "bibliothèques d’éléments d’IU standard pour Android et iOS. "
                             "Flutter reste cependant aussi adapté au développement d’applications web de bureau classiques. "
-                            "Les applications développées avec prennent l’aspect d’applications typiques des systèmes correspondants (Android ou IOS) "
-                            "et se comportent également de manière similaire, sans avoir besoin de prêter attention à ces caractéristiques. "
                             "Flutter utilise le language Dart, qui à l'aide de notions telles que Media Query ou Aspect Ratio, "
-                            "va récuperer les caractéristiques de l'objet utilisé afin d'y adapter l'application",
+                            "va récuperer les caractéristiques de l'objet utilisé afin d'y adapter l'application.",
+                        style: TextStyle(
+                          fontSize : 20,
+                          color: Colors.black,
+                        ),
+                        children: <TextSpan>[
+                          TextSpan(text: 'Son fonctionnement sous forme de Widget rends sont utilisation des plus intuitive.'
+                              'Par exemple, tout ce que vous voyez sur cette page, le texte, la barre de navication, le titre, le bouton "Home"... '
+                              "Ce sont tout des Widgets. Ce qui m'a permis de créer une application simple en un weekend, sans grandes connaissances en C."),
+                          TextSpan(text: '')
+                        ]
+                    ),),),),),),],),
+    ),
+    SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 30,
+          ),
+          SizedBox(
+            //width: MediaQuery.of(context).size.width, height: MediaQuery.of(context).size.height/5,
+            child: Center(
+              child: Text('React Native',
+                  style: TextStyle (
+                      fontSize: 25, fontWeight: FontWeight.bold, color: Colors.deepPurple[600])
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          SizedBox( //width: MediaQuery.of(context).size.width/1.05, height: MediaQuery.of(context).size.height/2.8,
+            child: Container(
+              decoration: myBoxDecoration(),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Center(
+                  child: RichText(
+                    textAlign: TextAlign.justify,
+                    text: TextSpan(
+                        text: 'React Native est aussi un framework multiplateformes disponible en open source, mais lui est créer par Facebook.'
+                            'Il se base sur React, et donc sur des libraires en JavaScript. '
+                            "Il s'exécute dans un processus en arrière-plan directement sur le terminal et communique avec la plate-forme native via une passerelle. "
+                            "Les applications React Native sont écrites à l'aide d'un mélange de balisage JavaScript et XML, connu sous le nom de JSX."
+                            "Ce qui s'approche des Widgets de Flutter mais reste plus compliquer à reverse 'engineering'",
                         style: TextStyle(
                           fontSize : 20,
                           color: Colors.black,
@@ -481,42 +523,6 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
         SizedBox(
           //width: MediaQuery.of(context).size.width, height: MediaQuery.of(context).size.height/5,
           child: Center(
-            child: Text('React Native',
-                style: TextStyle (
-                    fontSize: 25, fontWeight: FontWeight.bold, color: Colors.deepPurple[600])
-            ),
-          ),
-        ),
-        SizedBox(
-          height: 30,
-        ),
-        SizedBox( //width: MediaQuery.of(context).size.width/1.05, height: MediaQuery.of(context).size.height/2.8,
-          child: Container(
-            decoration: myBoxDecoration(),
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Center(
-                child: RichText(
-                  textAlign: TextAlign.justify,
-                  text: TextSpan(
-                      text: 'Comparer la nouvelle version de Flutter aux autres outils permettant de créer des applications mobiles cross-platform, disponibles sur le marché. ',
-                      style: TextStyle(
-                        fontSize : 20,
-                        color: Colors.black,
-                      ),
-                      children: <TextSpan>[
-                        TextSpan(text: '\nFlutter serait-elle une solution plus adaptée que React Native pour le travail de Junior ISEP?'),
-                        TextSpan(text: '')
-                      ]
-                  ),),),),),),],),
-    Column(
-      children: <Widget>[
-        SizedBox(
-          height: 30,
-        ),
-        SizedBox(
-          //width: MediaQuery.of(context).size.width, height: MediaQuery.of(context).size.height/5,
-          child: Center(
             child: Text('Comparaison',
                 style: TextStyle (
                     fontSize: 25, fontWeight: FontWeight.bold, color: Colors.deepPurple[600])
@@ -526,26 +532,93 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
         SizedBox(
           height: 30,
         ),
+        SizedBox(
+          height: 30,
+          child: Text(
+            "Un tableau dans une application, c'est déjà plus créatif non?"
+          ),
+        ),
         SizedBox( //width: MediaQuery.of(context).size.width/1.05, height: MediaQuery.of(context).size.height/2.8,
           child: Container(
             decoration: myBoxDecoration(),
             child: Padding(
               padding: const EdgeInsets.all(16.0),
               child: Center(
-                child: RichText(
-                  textAlign: TextAlign.justify,
-                  text: TextSpan(
-                      text: 'Comparer la nouvelle version de Flutter aux autres outils permettant de créer des applications mobiles cross-platform, disponibles sur le marché. ',
-                      style: TextStyle(
-                        fontSize : 20,
-                        color: Colors.black,
-                      ),
-                      children: <TextSpan>[
-                        TextSpan(text: '\nFlutter serait-elle une solution plus adaptée que React Native pour le travail de Junior ISEP?'),
-                        TextSpan(text: '')
-                      ]
-                  ),),),),),),],),
+                child: Column(children: <Widget>[
+                            Container(
+                              margin: EdgeInsets.all(20),
+                              child: Table(
+                                border: TableBorder.all(
+                                    color: Colors.black,
+                                    style: BorderStyle.solid,
+                                    width: 2),
+                                children: [
+                                  TableRow( children: [
+                                    Column(children:[Text(' ')]),
+                                    Column(children:[Text('Flutter', style: TextStyle(fontSize: 20.0))]),
+                                    Column(children:[Text('React Native', style: TextStyle(fontSize: 20.0))]),
+                                  ]),
+                                  TableRow( children: [
+                                    Column(children:[Text("Qu'est-ce que c'est?")]),
+                                    Column(children:[Text("Une boîte à outils d'interface utilisateur portable pour créer des applications compilées en natif sur mobile, Web et bureau à partir d'une seule base de code")]),
+                                    Column(children:[Text("Un framework pour créer des applications natives à l'aide de React")]),
+                                  ]),
+                                  TableRow( children: [
+                                    Column(children:[Text("Créé par")]),
+                                    Column(children:[Text("Google")]),
+                                    Column(children:[Text("Facebook")]),
+                                  ]),
+                                  TableRow( children: [
+                                    Column(children:[Text("Langage de programmation")]),
+                                    Column(children:[Text("Dart")]),
+                                    Column(children:[Text("JavaScript")]),
+                                  ]),
+                                  TableRow( children: [
+                                    Column(children:[Text("Chargement à Chaud")]),
+                                    Column(children:[Text("Oui")]),
+                                    Column(children:[Text("Oui")]),
+                                  ]),
+                                  TableRow( children: [
+                                    Column(children:[Text("UI")]),
+                                    Column(children:[Text("Les applications Flutter sont aussi bonnes sur les systèmes d'exploitation à jour que sur les anciennes versions."
+                                        "Comme elles n'ont qu'une seule base de code, les applications se présentent et se comportent de la même manière sur iOS et Android - mais grâce aux widgets Material Design et Cupertino, elles peuvent également imiter la conception de la plate-forme elle-même. Comment est-ce possible?"
+                                        "Flutter contient deux ensembles de widgets conformes à des langages de conception spécifiques: les widgets Material Design implémentent le langage de conception de Google du même nom; Les widgets Cupertino imitent le design iOS d'Apple."
+                                        " Cela signifie que votre application Flutter aura une apparence et un comportement naturels sur chaque plate-forme, imitant leurs composants natifs.")]),
+                                    Column(children:[Text("Les composants d'application ressemblent à ceux natifs (par exemple, un bouton sur un appareil iOS ressemble à un bouton iOS natif, et la même chose sur Android ). "
+                                      "Le fait que React Native utilise des composants natifs sous le capot devrait vous donner l'assurance qu'après toute mise à jour de l'interface utilisateur du système d'exploitation, les composants de votre application seront également mis à niveau instantanément."
+                                           "Cela dit, cela peut casser l'interface utilisateur de l'application, mais cela se produit très rarement."
+                                        "  Si vous voulez que votre application soit presque identique sur toutes les plates - formes - ainsi que sur les anciennes versions d'un système d'exploitation (comme Flutter le fait) - envisagez d'utiliser des bibliothèques tierces ( comme celle-ci ). Ils vous permettront d'utiliser des composants Material Design, à la place des composants natifs."
+                                    )]),
+                                  ]),
+                                  TableRow( children: [
+                                    Column(children:[Text("Avantage compétitif")]),
+                                    Column(children:[Center(
+                                      child: Text("Superbe apparence et convivialité grâce à de riches widgets;\n\n"
+                                          " Communauté et popularité en croissance rapide;\n"
+                                          "Excellente documentation avec le soutien solide de l'équipe Flutter (ce qui facilite le démarrage du développement avec Flutter);\n\n"
+                                          "Amélioration de Flutter pour le Web, offrant le potentiel d'une base de code unique sur les plates-formes mobiles et Web\n\n"
+                                          "Difficile à battre le délai de mise sur le marché"),
+                                    )]),
+                                    Column(children:[Text("Stabilité (5+ ans sur le marché);\n\n"
+                                        "De nombreux acteurs du marché éminents et prospères utilisent React Native;\n\n"
+                                        "Communauté mature et vaste;\n\n"
+                                        "Technologie facile à apprendre;\n\n"
+                                        "De nombreux didacticiels et bibliothèques, qui permettent un développement rapide et facile;\n\n"
+                                        "Le code peut être facilement réutilisé pour le développement d'applications Web et d'applications de bureau.")]),
+                                  ]),
+
+
+                                ],
+                              ),
+                            ),
+                          ]))
+
+                  ),
+  ),
+  ),
+  ]),
   ];
+
 
   void _onItemTapped(int index) {
     setState(() {
@@ -615,16 +688,19 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
             label: 'Context',
           ),
           BottomNavigationBarItem(
+            backgroundColor:  Color.fromARGB(255, 245, 127, 23),
             icon: Icon(Icons.looks_one_outlined,
             ),
             label: 'Flutter',
           ),
           BottomNavigationBarItem(
+            backgroundColor:  Color.fromARGB(255, 245, 127, 23),
             icon: Icon(Icons.looks_two_outlined,
             ),
             label: 'React',
           ),
           BottomNavigationBarItem(
+            backgroundColor:  Color.fromARGB(255, 245, 127, 23),
             icon: Icon(Icons.analytics_outlined,),
             label: 'Comparaison',
           ),
